6fa0bdcd8241bce07c4d24e33934d04194657ebb
Enzo Roiz <enzoroiz@gmail.com>
2021-05-10 20:15:03 +0200

    implementing delete saved article feature

--- NewsApp/app/src/main/java/com/enzoroiz/newsapp/SavedNewsFragment.kt
+++ NewsApp/app/src/main/java/com/enzoroiz/newsapp/SavedNewsFragment.kt
@@ -7,11 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.enzoroiz.newsapp.databinding.FragmentSavedNewsBinding
 import com.enzoroiz.newsapp.presentation.NewsAdapter
 import com.enzoroiz.newsapp.presentation.viewmodel.NewsViewModel
 import com.enzoroiz.newsapp.presentation.viewmodel.NewsViewModelFactory
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -28,6 +31,30 @@ class SavedNewsFragment : Fragment() {
         )
     }
 
+    private val swipeToDeleteCallback = object : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+        ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return true
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val article = adapter.differ.currentList[viewHolder.adapterPosition]
+            viewModel.deleteSavedArticle(article)
+            Snackbar.make(requireView(), getString(R.string.saved_news_fragment_message_article_deleted), Snackbar.LENGTH_LONG)
+                .setAction(getString(R.string.saved_news_fragment_button_undo_label)) {
+                    viewModel.saveArticle(article)
+                }
+                .show()
+        }
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,6 +67,8 @@ class SavedNewsFragment : Fragment() {
         binding = FragmentSavedNewsBinding.bind(view)
         binding.lstArticles.layoutManager = LinearLayoutManager(requireContext())
         binding.lstArticles.adapter = adapter
+
+        ItemTouchHelper(swipeToDeleteCallback).attachToRecyclerView(binding.lstArticles)
 
         viewModel.getSavedArticles().observe(viewLifecycleOwner, { articles ->
             adapter.differ.submitList(articles)
